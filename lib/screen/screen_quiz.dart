7f74a0eccfb6bbefe530986ff066da6e75133470
min02choi <min02choi@naver.com>
2023-09-06 16:47:39 +0900

    [Ch0-2] Create Quiz Model

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -1,5 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/model/model_quiz.dart';
+import 'package:flutter_application_1/widget/widget_candidate.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 
 class QuizScreen extends StatefulWidget {
@@ -31,10 +34,122 @@ class _QuizScreenState extends State<QuizScreen> {
             ),
             width: width * 0.85,
             height: height * 0.5,
-            // child: Swiper(phy),
+            child: Swiper(
+              physics: NeverScrollableScrollPhysics(),
+              loop: false,
+              itemCount: widget.quizzes.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildQuizCard(widget.quizzes[index], width, height);
+              },
+            ),
           ),
         ),
       ),
     );
+  }
+  Widget _buildQuizCard(Quiz quiz, double width, double height) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.white),
+        color: Colors.white
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q' + (_currentIndex + 1).toString() + '.',
+              style: TextStyle(
+                fontSize: width * 0.06,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                fontSize: width * 0.048,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            children: _buildCandidates(width, quiz),
+          ),
+          Container(
+            padding: EdgeInsets.all(width * 0.024),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.5,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ElevatedButton(
+                  child: _currentIndex == widget.quizzes.length - 1
+                      ? const Text('결과보기')
+                      : Text('다음문제'),
+                      
+                  // textColor: Colors.white,
+                  // color: Colors.deepPurple,
+                  onPressed: _answers[_currentIndex] == -1
+                      ? null
+                      : () {
+                          if (_currentIndex == widget.quizzes.length - 1) {
+                          } else {
+                            _answerState = [false, false, false, false];
+                            _currentIndex += 1;
+                          }
+                        },
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+    for (int i = 0; i < 4; i++) {
+      _children.add(
+        CandWidget(
+          index: i,
+          text: quiz.candidates[i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                  // print(_answers[_currentIndex]);
+                }
+                else {
+                  _answerState[j] = false;
+                }
+              }
+            });
+          },
+        ),
+      );
+      _children.add(
+        Padding(
+          padding: EdgeInsets.all(width * 0.024),
+        ),
+      );
+    }
+    return _children;
   }
 }
